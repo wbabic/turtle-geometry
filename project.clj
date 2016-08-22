d81c04fb53c1b2a3c87e10083a7836ba4c41a5cc
Walter P Babic <wbabic@mac.com>
2016-08-22 03:28:39 -0600

    updated clojure dependency

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha11"]
                  [org.clojure/clojurescript "1.9.216"]
 
-                 [devcards "0.2.1-7" :exclusions [org.clojure/tools.reader
-                                                  cljsjs/react
+
+                 [devcards "0.2.1-7" :exclusions [cljsjs/react
                                                   cljsjs/react-dom
                                                   cljsjs/react-dom-server]]
                  [sablono "0.7.4"]
